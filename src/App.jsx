284723607c7733c28c2d959f47643ac34eba90e6
Afrad Bhuiyan <afradbhuiyan2021@gmail.com>
2023-05-15 18:11:42 +0600

    refactor: update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import './assets/css/app.css';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router/Router';
-import Login from './pages/Login';
 
 function App() {
-  return <Login />;
   return (
     <BrowserRouter>
       <Router />
