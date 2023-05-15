db0c68785f2aca80d26fbdc73e9fe603211b8319
Afrad Bhuiyan <afradbhuiyan2021@gmail.com>
2023-05-15 14:34:15 +0600

    refactor: render login page directly from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './assets/css/app.css';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router/Router';
+import Login from './pages/Login';
 
 function App() {
+  return <Login />;
   return (
     <BrowserRouter>
       <Router />
